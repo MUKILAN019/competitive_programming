671a725ec775349a95c1d4b88635e65418990e9a
MUKILAN019 <mukilan.p@kalvium.community>
2024-08-12 22:44:47 +0530

    added day 11

---
+++ Day-11/SubSets.py
@@ -0,0 +1,15 @@
+def subsets(nums):
+    result = []
+    def backtrack(start, path):
+        result.append(path[0:])
+        for i in range(start, len(nums)):
+            path.append(nums[i])
+            backtrack(i + 1, path)
+            path.pop()
+    
+    backtrack(0, [])
+    return result
+
+
+nums = [1, 2, 3]
+print(subsets(nums))  
